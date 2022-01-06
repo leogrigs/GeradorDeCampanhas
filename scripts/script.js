c2e560230d354cf54cb58efca444e343f213de19
Leonardo Grigorio Ferreira <lgrigori@emeal.nttdata.com>
2022-01-06 12:58:25 -0300

    Alteração na validação de campos

--- scripts/script.js
+++ scripts/script.js
@@ -123,55 +123,39 @@ function criaCampoTexto(texto){
 }
 
 function criaCampoNome() {
-  $(".formulario").append("<div class='mb-3 row'><label for='nome' class='col-sm-2 col-form-label'>Nome</label><div class='col-sm-10'><input type='text' class='form-control' id='nome' onblur='verificaNome(this.value)' placeholder='Nome Completo [max 50]' maxlength='50'></div></div>")
+  $(".formulario").append("<div class='mb-3 row'><label for='nome' class='col-sm-2 col-form-label'>Nome</label><div class='col-sm-10'><input type='text' class='form-control' pattern='[a-zA-Z]*' id='nome' required onblur='verificaNome(this.value)' placeholder='Nome Completo [max 50]' maxlength='50'></div></div>")
   
 }
 function criaCampoCPF() {
-  $(".formulario").append("<div class='mb-3 row'><label for='cpf' class='col-sm-2 col-form-label'>CPF</label><div class='col-sm-10'><input type='text' class='form-control' id='cpf' onke placeholder='CPF' onkeyup='verificaCPF()'></div></div>")
+  $(".formulario").append("<div class='mb-3 row'><label for='cpf' class='col-sm-2 col-form-label'>CPF</label><div class='col-sm-10'><input type='text' class='form-control' pattern='[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}' id='cpf' required onke placeholder='CPF' onkeyup='verificaCPF()'></div></div>")
 }
 function criaCampoApelido() {
-  $(".formulario").append("<div class='mb-3 row'><label for='apelido' class='col-sm-2 col-form-label'>Apelido</label><div class='col-sm-10'><input type='text' class='form-control' id='apelido' onblur='verificaApelido(this.value)' placeholder='Apelido [max 20]' maxlength='20'></div></div>")
+  $(".formulario").append("<div class='mb-3 row'><label for='apelido' class='col-sm-2 col-form-label'>Apelido</label><div class='col-sm-10'><input type='text' class='form-control' pattern='[a-zA-Z]*' id='apelido' required onblur='verificaApelido(this.value)' placeholder='Apelido [max 20]' maxlength='20'></div></div>")
 }
 function criaCampoEmail() {
-  $(".formulario").append("<div class='mb-3 row'><label for='email' class='col-sm-2 col-form-label'>Email</label><div class='col-sm-10'><input type='text' class='form-control col-sm-5' id='email' onblur='verificaEmail(this.value)' placeholder='Email' maxlength='50'></div></div>")
+  $(".formulario").append("<div class='mb-3 row'><label for='email' class='col-sm-2 col-form-label'>Email</label><div class='col-sm-10'><input type='email' class='form-control col-sm-5' id='email' required onblur='verificaEmail(this.value)' placeholder='Email' maxlength='50'></div></div>")
 }
 function criaCampoData() {
-  $(".formulario").append("<div class='mb-3 row'><label for='data' class='col-sm-2 col-form-label'>Data de Nascimento</label><div class='col-sm-10'><input type='text' class='form-control' id='data' onke placeholder='dd/mm/yyyy' onkeyup='verificaData()'></div></div>")
+  $(".formulario").append("<div class='mb-3 row'><label for='data' class='col-sm-2 col-form-label'>Data de Nascimento</label><div class='col-sm-10'><input type='date' class='form-control' id='data' required onke placeholder='dd/mm/yyyy' onkeyup='verificaData()'></div></div>")
 }
 function criaCampoCEP() {
-  $(".formulario").append("<div class='mb-3 row'><label for='cep' class='col-sm-2 col-form-label'>CEP</label><div class='col-sm-10'><input type='text' class='form-control' id='cep' placeholder='CEP' onkeyup='verificaCEP()'></div></div>")
+  $(".formulario").append("<div class='mb-3 row'><label for='cep' class='col-sm-2 col-form-label'>CEP</label><div class='col-sm-10'><input type='text' class='form-control' pattern='[0-9]{5}\-[0-9]{3}' id='cep' required placeholder='CEP' onkeyup='verificaCEP()'></div></div>")
 }
 function criaCampoCheck(pergunta){
   console.log(pergunta);
   $(".formulario").append(`<div class='mb-3 row'><div class='col-sm-2'><input class='form-check-input' type='checkbox' value='' id='flexCheckDefault'></div><div class='col-sm-10'><label class='form-check-label' for='flexCheckDefault'><h6>${pergunta}</h6></label></div></div> `)
 }
 function criaCampoWhats() {
-  $(".formulario").append("<div class='mb-3 row'><label for='whats' class='col-sm-2 col-form-label'>Whatsapp</label><div class='col-sm-10'><input type='text' class='form-control' id='whats' onke placeholder='(DDD) Telefone' onkeyup='verificaWhats()'></div></div>")
+  $(".formulario").append("<div class='mb-3 row'><label for='whats' class='col-sm-2 col-form-label'>Whatsapp</label><div class='col-sm-10'><input type='text' class='form-control' minlength='15' id='whats' required placeholder='(DDD) Telefone' onkeyup='verificaWhats()'></div></div>")
 }
 function criaImagem(urlImagem){
   $(".imagem").append( `<img class='camp' src='${urlImagem}' alt='imagem campanha'>`)
 }
 
 // Funções de verificação de campos e máscaras
-function verificaNome(nome) {
-  var regex = new RegExp("[^0-9]")
-  if (!regex.test(nome)){
-    alert("Nome Inválido");
-  }
-}
 function verificaCPF() {
   var $cpf = $("#cpf");
   $cpf.mask('000.000.000-00', {reverse: true});
-}
-function verificaApelido(apelido) {
-  var regex = new RegExp("[^0-9]")
-  if (!regex.test(apelido)){
-    alert("Apelido Inválido");
-  }
-}
-function verificaData() {
-  var $data = $("#data");
-  $data.mask('00/00/0000');
 }
 function verificaCEP() {
   var $cep = $("#cep");
@@ -187,7 +171,10 @@ function verificaEmail(email) {
 }
 function verificaWhats() {
   var $whats = $("#whats");
-  $whats.mask('(000) 00000-0000');
+  $whats.mask('(00) 00000-0000');
+}
+function validar() {
+  $("#MainForm").validate();
 }
 
 
